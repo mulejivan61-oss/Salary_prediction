a992aa766b5dbb21bc51f847438da546dba0ed0b
mulejivan61-oss <mulejivan61@gmail.com>
2026-05-31 23:05:59 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+model = joblib.load("salary_model.pkl")
+scaler = joblib.load("scaler.pkl")
+
+st.title("Salary Prediction App")
+
+experience = st.number_input("Experience")
+education = st.number_input("Education Level")
+skills = st.number_input("Skills Count")
+certifications = st.number_input("Certifications")
+projects = st.number_input("Projects Done")
+interview = st.number_input("Interview Score")
+
+if st.button("Predict Salary"):
+
+    skill_efficiency = skills / experience if experience != 0 else 0
+
+    features = np.array([[experience, education, skills,
+                          certifications, projects,
+                          interview, skill_efficiency]])
+
+    scaled = scaler.transform(features)
+
+    prediction = model.predict(scaled)
+
+    st.success(f"Predicted Salary: ₹{prediction[0]:,.2f}")
